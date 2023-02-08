cace74689e575785e407f4628c0399c6a594c7fa
Donovanbkf <oscarbkf1@gmail.com>
2023-02-08 11:44:00 +0100

    validación skills y rutas

--- src/routes/skills.js
+++ src/routes/skills.js
@@ -1,48 +1,46 @@
 const express = require('express');
-
+const { matchedData } = require("express-validator");
 const Skills = require('../models/skills');
 const checkAuth = require('../middleware/auth')
 const router = express.Router()
+const  {validateSkillCreate, validateSkillEdit} = require('../validators/skills')
 
-
-router.get('/list', async (req, res)=> {
+router.get('/list', checkAuth(), async (req, res)=> {
     const skills = await Skills.findAll({raw:true})
-    console.log(skills)
-    res.render('skills/all-skills', {skills});
+    res.send(skills)
+    // res.render('skills/all-skills', {skills});
 })
 
-router.get('/new-skill', checkAuth ,(req, res)=> {
+router.get('/new-skill', checkAuth("admin") ,(req, res)=> {
     res.render('skills/new-skill');
 })
 
-router.post('/new-skill', checkAuth ,async (req, res)=> {
-    console.log(req.body);
-    // if (req.user.role === 'user'){
-    //     res.redirect('/skills/list');
-    // }
-    await Skills.create(req.body)
+router.post('/new-skill', checkAuth("admin"), validateSkillCreate, async (req, res)=> {
+    // console.log(req.body);
+    req = matchedData(req)
+    // console.log(req)
+    await Skills.create(req)
     res.redirect('/skills/list');
 })
 
-router.get('/edit-skill/:id', async (req, res)=> {
+router.get('/edit-skill/:id', checkAuth("admin"), async (req, res)=> {
     console.log(req.params);
     const skill = await Skills.findOne({raw:true, where: {id: req.params.id}})
     res.render('skills/edit-skill', {skill});
 })
 
-router.post('/edit-skill/:id', async (req, res)=> {
-    console.log(req.params);
-    const asignatura = req.body.asignature
-    const skill = await Skills.update({asignature: asignatura},{where: {id: req.params.id}})
-    console.log(skill);
-    res.redirect('/list');
+router.post('/edit-skill/:id', checkAuth("admin"), validateSkillEdit, async (req, res)=> {
+    let id = req.params.id
+    req = matchedData(req)
+    const skill = await Skills.update({asignature: req.asignature},{where: {id: id}})
+    res.send(skill);
 })
 
-router.post('/delete-skill/:id', async (req, res)=> {
-    console.log(req.params);
+router.post('/delete-skill/:id', checkAuth("admin"), async (req, res)=> {
+    // console.log(req.params);
     const skill = await Skills.destroy({where: {id: req.params.id}})
-    console.log('deleted  deekiedjijdiewj ', skill);
-    res.send('lolalolooo');
+    // console.log('deleted  deekiedjijdiewj ', skill);
+    res.send(skill);
 })
 
 
---
+++ src/validators/skills.js
@@ -0,0 +1,33 @@
+const { check } = require('express-validator')
+const { validateResult } = require("../helpers/helperValidator");
+const Skills = require('../models/skills');
+
+const validateSkillCreate = [
+  check("asignature").exists().notEmpty(),
+  (req, res, next) => {
+    validateResult(req, res, next);
+  },
+];
+
+const validateSkillEdit = [
+  check("asignature").exists().notEmpty(),
+  // check("id").custom(async (value, {req}) => {
+  //   const skill = await Skills.findOne({raw:true, where: { id: value } });
+  //   req.skill = skill;
+  //   if (!skill){ 
+  //      throw new Error(`Skill dont exists`);
+  //   }
+  //   return true;
+  // }),
+  // check("rol").custom((value, {req}) => {
+  //   if (value != "admin"){ 
+  //      throw new Error(`No eres admin`);
+  //   }
+  //   return true;
+  // }),
+  (req, res, next) => {
+    validateResult(req, res, next);
+  },
+];
+
+module.exports = {validateSkillCreate, validateSkillEdit}
