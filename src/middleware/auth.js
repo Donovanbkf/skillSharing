79716761f4b672d7c3885db2296fee5906f45917
Donovanbkf <oscarbkf1@gmail.com>
2023-02-08 11:44:47 +0100

    auth del rol

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,16 +1,21 @@
 const jwt = require('jsonwebtoken');
 
 
-const checkAuth = (req, res, next) => {
-
+const checkAuth = (rol) => (req, res, next) => {
+// const checkAuth = (req, res, next) => {
+  // console.log(rol)
   if (!req.headers.authorization) {
-      res.send('mal')
-    return;
+    return res.send('no estas autorizado')
   }
-  console.log(req.headers.authorization);
+  // console.log(req.headers.authorization);
   const token = req.headers.authorization.split(" ").pop();
   const decoded = jwt.verify(token, process.env.JWT_key);
-  console.log(decoded);
+  // console.log(decoded);
+  if(rol == 'admin'){
+    if (decoded.rol != 'admin'){
+      return res.send('no tienes los derechos')
+    }
+  }
   req.user = decoded
   next();
 };
