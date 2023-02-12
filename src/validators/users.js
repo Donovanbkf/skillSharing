643c909da690328edf78b3549db6436c319b56e1
Donovanbkf <oscarbkf1@gmail.com>
2023-02-12 23:27:14 +0100

    validación mejorada

--- src/validators/users.js
+++ src/validators/users.js
@@ -4,38 +4,40 @@ const Users = require('../models/users');
 const {compare} = require('../helpers/handleBcrypt')
 
 const validateUserCreate = [
-  check("name").exists().notEmpty(),
-  check("username").exists().notEmpty().custom(async (value, {req}) => {
+  body("name").exists().withMessage('name no recibido').isLength({min:5, max:20}).withMessage('tamaño incorrecto'),
+  body("username").exists().withMessage('username no recibido').isLength({min:5, max:20}).withMessage('tamaño incorrecto').custom(async (value, {req}) => {
     const user = await Users.findOne({raw:true, where: { username: value } });
     if (user){
-      req.status(409)
+      req.status = 409
       throw new Error(`User ${value} already exists`);
     }
     return true;
   }),
-  check("fullname").exists().notEmpty(),
-  check("password").exists().notEmpty(),
-  check("role").exists().notEmpty(),
+  body("fullname").exists().withMessage('fullname no recibido').notEmpty().isLength({min:5, max:20}).withMessage('tamaño incorrecto'),
+  body("password").exists().withMessage('password no recibido').notEmpty(),
+  body("role").isIn(["user", "admin"]).exists().withMessage('role no recibido'),
   (req, res, next) => {
     validateResult(req, res, next);
   },
 ];
 
 const validateUserLogin = [
-    body("username").exists().withMessage('username no recibido').notEmpty(),
-    body("password").exists().notEmpty().custom(async (value, {req}) => {
-        const user = await Users.findOne({raw:true, where: { username: req.body.username } });
-        req.user = user;
-        if (!user){
-          req.status = 404;
-          throw new Error(`User ${req.body.username} dont exists`);
-        }else{
-          if(!await compare(value, user.password)){
-            req.status = 401;
-            throw new Error(`Contraseña no valida`);
-          }
-        }
-        return true;
+    body("username").exists().withMessage('username no recibido').notEmpty().withMessage('username vacío').custom(async (value, {req}) => {
+      const user = await Users.findOne({raw:true, where: { username: value } });
+      req.user = user;
+      if (!user){
+        req.status = 404
+        throw new Error(`User ${value} dont exists`);
+      }
+      return true;
+    }),
+    body("password").exists().withMessage('password no recibido').notEmpty().custom(async (value, {req}) => {
+      const user = req.user;
+      if(!await compare(value, user.password)){
+        req.status = 401;
+        throw new Error(`Contraseña no valida`);
+      }     
+      return true;
       }),
     (req, res, next) => {
       validateResult(req, res, next);
