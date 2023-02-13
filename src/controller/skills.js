a3371a353fcd5562f2f3f0df587146951562aaff
Donovanbkf <oscarbkf1@gmail.com>
2023-02-13 19:29:21 +0100

    status skills

--- src/controller/skills.js
+++ src/controller/skills.js
@@ -1,37 +1,31 @@
 const Skills = require("../models/skills")
 const { matchedData } = require("express-validator");
 
+
 const listar = async (req, res)=> {
     const skills = await Skills.findAll({raw:true})
-    res.send(skills)
-    // res.render('skills/all-skills', {skills});
-}
-
-const get_new_skill = (req, res)=> {
-    res.render('skills/new-skill');
+    if (responses.length > 0) {
+        return res.status(200).send(skills)
+    }
+    res.status(204).send(skills)
 }
 
 const new_skill = async (req, res)=> {
     req = matchedData(req)
-    await Skills.create(req)
-    res.redirect('/skills/list');
-}
-
-const get_edit_skill = async (req, res)=> {
-    const skill = await Skills.findOne({raw:true, where: {id: req.params.id}})
-    res.render('skills/edit-skill', {skill});
+    const skills = await Skills.create(req)
+    res.status(201).send(skills)
 }
 
 const edit_skill = async (req, res)=> {
     let id = req.params.id
     req = matchedData(req)
     const skill = await Skills.update({asignature: req.asignature, description: req.description},{where: {id: id}})
-    res.send(skill);
+    res.status(200).send(skill);
 }
 
 const delete_skill = async (req, res)=> {
     const skill = await Skills.destroy({where: {id: req.params.id}})
-    res.send('skill');
+    res.status(204).send(skill);
 }
 
 module.exports = { listar, get_new_skill, new_skill, get_edit_skill, edit_skill, delete_skill }
--- src/routes/skills.js
+++ src/routes/skills.js
@@ -1,17 +1,13 @@
 const express = require('express');
-
 const checkAuth = require('../middleware/auth')
 const router = express.Router()
 const  {validateSkillCreate, validateSkillEdit} = require('../validators/skills');
-const { listar, get_new_skill, new_skill, get_edit_skill, edit_skill, delete_skill} = require('../controller/skills');
+const { listar, new_skill, edit_skill, delete_skill} = require('../controller/skills');
+
 
 router.get('/list', checkAuth(), listar)
 
-router.get('/new-skill', checkAuth("admin") , get_new_skill)
-
 router.post('/new-skill', checkAuth("admin"), validateSkillCreate, new_skill)
-
-router.get('/edit-skill/:id', checkAuth("admin"), get_edit_skill)
 
 router.post('/edit-skill/:id', checkAuth("admin"), validateSkillEdit, edit_skill)
 
