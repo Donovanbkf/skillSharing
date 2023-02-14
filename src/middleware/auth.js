7eeba059f044a4a9b016de30135f55b7c61c759c
Donovanbkf <oscarbkf1@gmail.com>
2023-02-14 15:08:52 +0100

    estado auth middle

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,13 +3,13 @@ const jwt = require('jsonwebtoken');
 
 const checkAuth = (rol) => (req, res, next) => {
   if (!req.headers.authorization) {
-    return res.send('no estas autorizado')
+    return res.status(401).send('no estas autorizado')
   }
   const token = req.headers.authorization.split(" ").pop();
   const decoded = jwt.verify(token, process.env.JWT_key);
   if(rol == 'admin'){
     if (decoded.rol != 'admin'){
-      return res.send('no tienes los derechos')
+      return res.status(403).send('no tienes los derechos')
     }
   }
   req.user = decoded
