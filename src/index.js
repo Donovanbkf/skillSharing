9c947774506170584e51e20347e3814d3eea28b5
Donovanbkf <oscarbkf1@gmail.com>
2023-02-15 11:47:51 +0100

    index mas simple y limpio

--- src/index.js
+++ src/index.js
@@ -16,21 +16,11 @@ dbConect()
 app.set('trust proxy', true);
 
 
-// config view engine
-const hbs = handlebars.create({
-  defaultLayout: "main",
-  layoutsDir: path.join(app.get("views"), "layouts"),
-  partialsDir: path.join(app.get("views"), "partials"),
-  extname: ".hbs",
-});
-app.engine(".hbs", hbs.engine);
-app.set("view engine", ".hbs");
-
 // middleware
 app.use(morgan('dev'))
-// app.use(morgan('combined'))
 app.use(express.json()) // para postman JSON hace que funcione ¿?
-app.use(express.urlencoded({ extended: true})) // para los form hace que funcione ¿? y son true elimina el [Object: null prototype] { }
+app.use(express.urlencoded({ extended: true})) // para los form hace que funcione ¿? y son true elimina el [Object: null prototype] { } 
+//extended false es mas simple y el true permite objetos anidados enn la peticion del formulario
 
 
 // rutas
